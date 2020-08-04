39ed3f80ea14af969f4564219e68d6712a2ae6d6
Vasya231 <gg231589@gmail.com>
2020-08-04 18:05:47 +0300

    load page and save it as index.html

--- src/bin/page-loader.js
+++ src/bin/page-loader.js
@@ -2,11 +2,13 @@
 
 import program from 'commander';
 
+import runApp from '../index';
+
 program.version('0.0.1')
   .description('Loads and save web page from internet')
   .option('--output [path]', 'set output directory', process.cwd())
   .arguments('<url>')
   .action((url, cmdObj) => {
-    console.log(url, cmdObj.output);
+    runApp(url, cmdObj.output);
   });
 program.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import { promises as fs } from 'fs';
 import path from 'path';
+import axios from 'axios';
+import nodeAdapter from 'axios/lib/adapters/http';
 
 export default (url, pathToDir) => {
-  console.log(pathToDir);
-  fs.writeFile(path.join(pathToDir, 'index.html'), '!!!');
+  axios.defaults.adapter = nodeAdapter;
+  return axios.get(url).then((response) => {
+    console.log(response.data);
+    fs.writeFile(path.join(pathToDir, 'index.html'), response.data);
+  }).catch((error) => console.log(error));
 };
