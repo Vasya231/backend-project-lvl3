c10a976d283e564cc3a4d401f7ab22befd092972
Vasya231 <gg231589@gmail.com>
2020-08-07 18:13:14 +0300

    add saving page and resources on disk

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import path from 'path';
 import axios from 'axios';
 import nodeAdapter from 'axios/lib/adapters/http';
 import cheerio from 'cheerio';
-import { uniq } from 'lodash';
+import { uniq, keyBy } from 'lodash';
 
 import { generateLocalFileName, generateResourceDirName, generateResourceFileName } from './utils';
 
@@ -22,12 +22,6 @@ const tags = {
   },
 };
 
-const getResourcePaths = ($, tagName) => {
-  const { linkAttr } = tags[tagName];
-  const links = $(tagName).map((i, element) => $(element).attr(linkAttr)).get();
-  return links.filter((resourcePath) => (resourcePath !== undefined));
-};
-
 export default (urlString, pathToDir) => {
   const testMode = process.env.TEST_MODE;
   if (testMode === 'true') {
@@ -37,47 +31,90 @@ export default (urlString, pathToDir) => {
   const url = new URL(urlString);
   const baseUrl = url.origin;
   const pageFileName = generateLocalFileName(url);
-  const pageFilePath = path.join(pageFileName, pathToDir);
+  const pageFilePath = path.join(pathToDir, pageFileName);
   const resourceDirName = generateResourceDirName(url);
-  const resourceDirPath = path.join(resourceDirName, pathToDir);
+  const resourceDirPath = path.join(pathToDir, resourceDirName);
+
+  console.log(pageFilePath);
+  console.log(resourceDirPath);
+
+  let localResourceMap;
+  let $;
 
   const isLocal = (pathToResource) => {
     const fullUrl = new URL(pathToResource, baseUrl);
     return (fullUrl.hostname === url.hostname);
   };
 
+  const transformLinks = (tagName, resourceMap) => {
+    const { linkAttr } = tags[tagName];
+    $(tagName).each((i, element) => {
+      const resourcePath = $(element).attr(linkAttr);
+      if (!resourcePath) {
+        return;
+      }
+      const resourceProps = resourceMap[resourcePath];
+      if (!resourceProps) {
+        return;
+      }
+      const { newLink } = resourceProps;
+      $(element).attr(linkAttr, newLink);
+    });
+  };
+
+  const getResourcePaths = (tagName) => {
+    const { linkAttr } = tags[tagName];
+    const links = $(tagName).map((i, element) => $(element).attr(linkAttr)).get();
+    return links.filter((resourcePath) => (resourcePath !== undefined));
+  };
+
+  const generateResourceMap = (resourcePaths) => {
+    const resourceProps = resourcePaths.map((resourcePath) => {
+      const dlLink = new URL(resourcePath, baseUrl).href;
+      const resourceFileName = generateResourceFileName(resourcePath);
+      const newLink = `${resourceDirName}/${resourceFileName}`;
+      return {
+        resourcePath,
+        dlLink,
+        newLink,
+        resourceFileName,
+      };
+    });
+    return keyBy(resourceProps, ({ resourcePath }) => resourcePath);
+  };
+
   return axios.get(url.href)
     .then((response) => {
-      const $ = cheerio.load(response.data);
+      $ = cheerio.load(response.data);
       const resourcePaths = Object.keys(tags).reduce(
-        (acc, tagName) => [...acc, ...getResourcePaths($, tagName)],
+        (acc, tagName) => [...acc, ...getResourcePaths(tagName)],
         [],
       );
       const uniqueResourcePaths = uniq(resourcePaths);
       const uniqueLocalResourcePaths = uniqueResourcePaths.filter(isLocal);
       console.log(uniqueLocalResourcePaths);
 
-      const resourceInfoObjs = uniqueLocalResourcePaths.map((resourcePath) => {
-        const dlLink = new URL(resourcePath, baseUrl).href;
-        const resourceFileName = generateResourceFileName(resourcePath);
-        const newLink = `${resourceDirName}/${resourceFileName}`;
-        return {
-          resourcePath,
-          dlLink,
-          resourceFileName,
-          newLink,
-        };
-      });
-      console.log(resourceInfoObjs);
+      localResourceMap = generateResourceMap(uniqueLocalResourcePaths);
+      console.log(localResourceMap);
 
-      const loadResource = (resourceInfoObj) => {
-        const { dlLink } = resourceInfoObj;
-        return axios.get(dlLink).then(({ data }) => ({
-          ...resourceInfoObj,
-          data,
-        }));
+      Object.keys(tags).forEach((tagName) => transformLinks(tagName, localResourceMap));
+
+      const loadResource = (resourceProps) => {
+        const { dlLink } = resourceProps;
+        return axios.get(dlLink).then(({ data }) => {
+          // eslint-disable-next-line no-param-reassign
+          resourceProps.data = data;
+          return true;
+        });
       };
-      return Promise.all(resourceInfoObjs.map(loadResource));
+      return Promise.all(Object.values(localResourceMap).map(loadResource));
     })
-    .then(console.log);
+    .then(() => fs.mkdir(resourceDirPath))
+    .then(() => {
+      const savePagePromise = fs.writeFile(pageFilePath, $().html);
+      const saveResourcePromises = Object.values(localResourceMap).map(
+        ({ resourceFileName, data }) => fs.writeFile(path.join(resourceDirPath, resourceFileName), data),
+      );
+      return Promise.all([savePagePromise, ...saveResourcePromises]);
+    });
 };
