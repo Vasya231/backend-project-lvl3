59af8f478c86f13861496f5193778443a4290be3
Vasya231 <gg231589@gmail.com>
2020-08-14 23:36:03 +0300

    add url validation to binary

--- src/bin/page-loader.js
+++ src/bin/page-loader.js
@@ -9,6 +9,13 @@ program.version('0.0.1')
   .option('--output [path]', 'set output directory', process.cwd())
   .arguments('<url>')
   .action((url, cmdObj) => {
+    try {
+      // eslint-disable-next-line no-new
+      new URL(url);
+    } catch (e) {
+      console.error('Invalid page url.');
+      process.exit(1);
+    }
     loadAndSavePage(url, cmdObj.output).catch((e) => {
       console.error(e.message);
       process.exit(1);
