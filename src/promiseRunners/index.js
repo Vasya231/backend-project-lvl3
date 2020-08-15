4da531ec4966f760e65b32c6e893e6e80bec6be3
Vasya231 <gg231589@gmail.com>
2020-08-15 22:23:00 +0300

    add promise runner that doesnt run promises

--- src/promiseRunners/index.js
+++ src/promiseRunners/index.js
@@ -4,6 +4,7 @@ import listrExecutor from './listr';
 const executors = {
   default: defaultExecutor,
   listr: listrExecutor,
+  none: (promises) => promises,
 };
 
 export default (executorType) => executors[executorType];
