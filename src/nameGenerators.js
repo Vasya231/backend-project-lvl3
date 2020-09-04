c76edc1993cc7bbac08e1cba760acab16e30e030
Vasya231 <gg231589@gmail.com>
2020-09-04 14:58:22 +0300

    refactor: remove flag from generatePrefix

--- src/nameGenerators.js
+++ src/nameGenerators.js
@@ -22,13 +22,13 @@ const parsePathname = (pathname) => {
   };
 };
 
-const generatePrefix = (url, isResource = false) => {
+const generatePrefix = (url, fileType = 'page') => {
   const { search, hostname, pathname } = url;
   const { pathnameParts, filename } = parsePathname(pathname);
   const formattedSearch = (search !== '')
     ? search.slice(0, search.length - 1)
     : '';
-  const prefixFromHostname = isResource ? '' : hostname;
+  const prefixFromHostname = (fileType === 'resource') ? '' : hostname;
   const prefixParts = [prefixFromHostname, ...pathnameParts, filename, formattedSearch];
   const transformedPrefix = prefixParts
     .filter((part) => part !== '')
@@ -52,7 +52,7 @@ export const getResourceFilenameGenerationFunction = () => {
   return (url) => {
     const { pathname } = url;
     const { extension } = parsePathname(pathname);
-    const transformedPrefix = generatePrefix(url, true);
+    const transformedPrefix = generatePrefix(url, 'resource');
     const transformedSuffix = extension ? `.${transformString(extension.slice(1))}` : '';
     const baseFilename = `${transformedPrefix}${transformedSuffix}`;
     const timesUsed = usedNames.get(baseFilename);
